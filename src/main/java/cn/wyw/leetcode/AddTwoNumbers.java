230be7793ba4aa3a7fa42532f0719f476d8d148a
wangyawu <540465314@qq.com>
2020-01-09 14:01:51 +0800

    add Two numbers

--- src/main/java/cn/wyw/leetcode/AddTwoNumbers.java
+++ src/main/java/cn/wyw/leetcode/AddTwoNumbers.java
@@ -1,7 +1,78 @@
 package cn.wyw.leetcode;
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
 /***
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ *
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ *
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ *
+ *  time : O(n)
+ *  space : O(n)
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-two-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
  * 2020/1/9 13:28
  */
 public class AddTwoNumbers {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        ListNode q1 = new ListNode(4);
+        ListNode q2 = new ListNode(3);
+        l1.next = q1;
+        q1.next = q2;
+
+        ListNode l2 = new ListNode(5);
+        ListNode w1 = new ListNode(6);
+        ListNode w2 = new ListNode(4);
+        l2.next = w1;
+        w1.next = w2;
+
+        ListNode listNode = addTwoNumbers(l1, l2);
+        while (listNode != null) {
+            System.out.print(listNode.val + " ");
+            listNode = listNode.next;
+        }
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode p1 = l1, p2 = l2, res = new ListNode(0);
+        ListNode cur = res;
+        int sum = 0;
+
+        while (p1 != null || p2 != null) {
+            if (p1 != null) {
+                sum += p1.val;
+                p1 = p1.next;
+            }
+            if (p2 != null) {
+                sum += p2.val;
+                p2 = p2.next;
+            }
+            cur.next = new ListNode(sum % 10);
+            sum /= 10;
+            cur = cur.next;
+        }
+        if (sum == 1) {
+            cur.next = new ListNode(1);
+        }
+        return res.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
 }
